0894b483b13c40e4b918c42aeac6b92e90071885
wangdongcheng <wangdongcheng@gmail.com>
2025-06-30 11:45:19 +0200

    Refactor SQL queries for consistency in formatting and readability

--- tech/SQL/access_mrs_research.sql
+++ tech/SQL/access_mrs_research.sql
@@ -1,50 +1,52 @@
 SELECT TOP (10)
     name
-from VJSCL.sys.tables
-where name LIKE 'ord\_______' ESCAPE '\'
+FROM VJSCL.sys.tables
+WHERE name LIKE 'ord\_______' ESCAPE '\'
 -- this is the order table
 ;
 
 SELECT TOP (10)
     OH_HEADER_REF, *
 FROM VJSCL.[dbo].[ORD_HEADER]
-where OH_ORDER_NUMBER = '767658'
+WHERE OH_ORDER_NUMBER = '767658'
 -- '767658' is the order number
 ;
 
 SELECT TOP (10)
     *
 FROM VJSCL.[dbo].[ORD_DETAIL]
-where OD_ORDER_NUMBER = '767658'
+WHERE OD_ORDER_NUMBER = '767658'
 -- '767658' is the order number
 ;
 
 SELECT TOP (10)
     *
 FROM VJSCL.dbo.SL_TRANSACTIONS -- invoice table
-where ST_HEADER_REF = '651978'
+WHERE ST_HEADER_REF = '651978'
 -- '651978' is the transaction header reference, the invoice number
 ;
 
 SELECT TOP (10)
     DET_STOCK_CODE,
     *
-from VJSCL.dbo.SL_PL_NL_DETAIL
-where DET_HEADER_REF = '651978'
+FROM VJSCL.dbo.SL_PL_NL_DETAIL
+WHERE DET_HEADER_REF = '651978'
 -- '651978' is the transaction header reference, the invoice number
 -- DET_PRIMARY is the audit No.
 ;
 
+-- Querying STK_STOCK to retrieve the main stock record for the given product code.
+-- This table may contain the primary stock information, whereas STK_STOCK_2 could store supplementary or historical data.
 SELECT TOP (10)
     *
-from VJSCL.dbo.STK_STOCK
+FROM VJSCL.dbo.STK_STOCK
 WHERE STKCODE = '30TMP_836945'
 ;
 
 SELECT TOP (10)
     *
-from VJSCL.dbo.STK_STOCK_2
-where STKCODE2 = '30TMP_836945'
+FROM VJSCL.dbo.STK_STOCK_2
+WHERE STKCODE2 = '30TMP_836945'
 -- '30TMP_836945' is the stock code, the product code
 ;
 
@@ -52,6 +54,6 @@ where STKCODE2 = '30TMP_836945'
 SELECT TOP (10)
     stk_usrchar5, -- this is the barcode
     *
-from VJSCL.dbo.STK_STOCK3
-where STKCODE3 = '30TMP_836945' -- '30TMP_836945' is the stock code, the product code
+FROM VJSCL.dbo.STK_STOCK3
+WHERE STKCODE3 = '30TMP_836945' -- '30TMP_836945' is the stock code, the product code
     ;
