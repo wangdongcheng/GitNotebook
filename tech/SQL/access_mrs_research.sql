8e25a6f9013ba2d0fdd87c677c5e4b48eeec9076
wangdongcheng <wangdongcheng@gmail.com>
2025-06-30 09:38:54 +0200

    1

--- tech/SQL/access_mrs_research.sql
+++ tech/SQL/access_mrs_research.sql
@@ -1,34 +1,32 @@
 SELECT TOP (10)
     OH_HEADER_REF, *
-FROM [VJSCL].[dbo].[ORD_HEADER]
-where OH_ORDER_NUMBER = '767658'
--- '767658' is the order number
+FROM VJSCL.[dbo].[ORD_HEADER]
+where OH_ORDER_NUMBER = '767658' -- '767658' is the order number
 ;
 
 SELECT TOP (10)
     *
 FROM VJSCL.dbo.SL_TRANSACTIONS
-where ST_HEADER_REF = '651978'
--- '651978' is the transaction header reference, the invoice number
+where ST_HEADER_REF = '651978' -- '651978' is the transaction header reference, the invoice number
 ;
 
 SELECT TOP (10)
     DET_stock_CODE,
     *
 from VJSCL.dbo.SL_PL_NL_DETAIL
-where DET_HEADER_REF = '651978'
+where DET_HEADER_REF = '651978' -- '651978' is the transaction header reference, the invoice number
 ;
 
 SELECT TOP (10)
     *
 from VJSCL.dbo.STK_STOCK_2
-where STKCODE2 = '30TMP_836945'
+where STKCODE2 = '30TMP_836945' -- '30TMP_836945' is the stock code, the product code
 ;
 
 
 SELECT TOP (10)
-    stk_usrchar5,
+    stk_usrchar5, -- this is the barcode
     *
 from VJSCL.dbo.STK_STOCK3
-where STKCODE3 = '30TMP_836945'
+where STKCODE3 = '30TMP_836945' -- '30TMP_836945' is the stock code, the product code
     ;
